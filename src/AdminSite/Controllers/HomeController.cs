e8687722e6abcd01e221870bd08266c47ac1ba69
FilippusD <34042536+FilippusD@users.noreply.github.com>
2024-10-15 15:50:00 +0000

    Updated logo and footer

--- src/AdminSite/Controllers/HomeController.cs
+++ src/AdminSite/Controllers/HomeController.cs
@@ -235,7 +235,7 @@ public class HomeController : BaseController
         this.logger.Info("Home Controller / Index ");
         try
         {
-            this.applicationConfigService.SaveFileToDisk("LogoFile", "contoso-sales.png");
+            this.applicationConfigService.SaveFileToDisk("LogoFile", "datacentral.svg");
             this.applicationConfigService.SaveFileToDisk("FaviconFile", "favicon.ico");
 
             try
